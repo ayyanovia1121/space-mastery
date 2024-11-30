d9b4c1198a2693af83578e796b551911d6089d34
ayyanovia1121 <noviayya1121@gmail.com>
2024-11-30 15:10:41 +0700

    Next installation and configure some package

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#FA7275",
+          100: "#EA6365",
+        }
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
