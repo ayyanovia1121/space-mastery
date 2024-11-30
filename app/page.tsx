79777617288e59ecf0ae7330e001252ddcd719bc
ayyanovia1121 <noviayya1121@gmail.com>
2024-11-30 17:08:10 +0700

    Auth Form

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Space Mastery - The only storage solution you need</h1>
+    <div className="flex-center h-screen">
+      <h1 className="text-3x font-bold text-brand-100">Space Mastery - The only storage solution you need</h1>
     </div>
   );
 }
