82e6624fb7d7b6b5ba10cb9eb635b1f63e62464c
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-23 15:18:55 +0700

    feat(layout): ✨ add protected layout with sidebar, header, and mobile navbar.

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,21 +1,27 @@
+import Header from "@/components/custom/header";
+import MobileNavbar from "@/components/custom/mobile-navbar";
+import Sidebar from "@/components/custom/sidebar";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import { Toaster } from "sonner";
 
+export const dynamic = "force-dynamic";
+const PtotectedLayout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
 
-const PtotectedLayout = ({ children }: { children: React.ReactNode }) => {
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <main className="flex h-screen">
-      {/* //TODO: Add sidebar */}
-      SIDEBAR
+      <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        {/* //TODO: MOBILE NAV */}
-        MOBILE NAVIGATION
-        {/* //TODO: HEADER */}
-        HEADER
+        <MobileNavbar {...currentUser} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
-      <Toaster richColors/>
+      <Toaster richColors />
     </main>
   );
-}
+};
 
-export default PtotectedLayout
+export default PtotectedLayout;
