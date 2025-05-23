6ba2d43851cfaafc76dfebf90e4b8dacea227d3d
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-23 13:06:46 +0700

    feat(actions): 🛠️ Implement user account creation with email OTP verification
    - Implements createAccount action to create user accounts.
    - Uses sendEmailOTP to send a one-time password to the user's email for verification.
    - Checks for existing users by email before creating a new account.
    - Creates a new user document in Appwrite with basic user information.

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { ID, Query } from "node-appwrite";
+import { parseStringify } from "../utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+    console.log(error, message);
+    throw error;
+};
+
+// Send Email OTP
+export const sendEmailOTP = async (email: string) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Error creating email token");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP(email);
+  if(!accountId) throw new Error("Failed to send OTP");
+
+  if(!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar: 
+        "https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80",
+        accountId,
+      }
+    )
+  }
+  return parseStringify({ accountId });
+};
