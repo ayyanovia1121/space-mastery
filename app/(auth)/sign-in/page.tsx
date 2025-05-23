07179e6716cfdd73dd6f38cad674439de974b4f1
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-23 13:05:29 +0700

    refactor(auth-form): ♻️ updates import paths for AuthForm in sign-in and sign-up pages to reflect its new location

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,6 @@
-import AuthForm from '@/components/AuthForm'
-import React from 'react'
+import AuthForm from "@/components/custom/AuthForm";
+import React from "react";
 
-const SignIn = () =>  <AuthForm type = "sign-in"/>
+const SignIn = () => <AuthForm type="sign-in" />;
 
-
-export default SignIn
+export default SignIn;
