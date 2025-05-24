3148c933dc79d8be1e42a808e6f30d94f2f3b632
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-24 21:11:50 +0700

    feat(constants): 🛠️ Add actions dropdown items to constants

--- constants/index.ts
+++ constants/index.ts
@@ -31,30 +31,57 @@ export const navItems = [
 export const avatarPlaceholderUrl =
   "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg";
 
-  export const sortTypes = [
-    {
-      label: "Date created (newest)",
-      value: "$createdAt-desc",
-    },
-    {
-      label: "Created Date (oldest)",
-      value: "$createdAt-asc",
-    },
-    {
-      label: "Name (A-Z)",
-      value: "name-asc",
-    },
-    {
-      label: "Name (Z-A)",
-      value: "name-desc",
-    },
-    {
-      label: "Size (Highest)",
-      value: "size-desc",
-    },
-    {
-      label: "Size (Lowest)",
-      value: "size-asc",
-    },
-  ];
-  
+export const sortTypes = [
+  {
+    label: "Date created (newest)",
+    value: "$createdAt-desc",
+  },
+  {
+    label: "Created Date (oldest)",
+    value: "$createdAt-asc",
+  },
+  {
+    label: "Name (A-Z)",
+    value: "name-asc",
+  },
+  {
+    label: "Name (Z-A)",
+    value: "name-desc",
+  },
+  {
+    label: "Size (Highest)",
+    value: "size-desc",
+  },
+  {
+    label: "Size (Lowest)",
+    value: "size-asc",
+  },
+];
+
+export const actionsDropdownItems = [
+  {
+    label: "Rename",
+    icon: "/assets/icons/edit.svg",
+    value: "rename",
+  },
+  {
+    label: "Details",
+    icon: "/assets/icons/info.svg",
+    value: "details",
+  },
+  {
+    label: "Share",
+    icon: "/assets/icons/share.svg",
+    value: "share",
+  },
+  {
+    label: "Download",
+    icon: "/assets/icons/download.svg",
+    value: "download",
+  },
+  {
+    label: "Delete",
+    icon: "/assets/icons/delete.svg",
+    value: "delete",
+  },
+];
